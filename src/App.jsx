61f4ff91414c4b8b75951039835b92e16feb8dec
Chase Moffat <chase@MacBook-Pro.local>
2024-03-12 20:17:35 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  const API_KEY = import.meta.env.VITE_APP_API_KEY;
+  
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
